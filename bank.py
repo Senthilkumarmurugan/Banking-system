92c131aca8c5d27b55c1cd8830448a1d80763710
Senthil Kumar Murugan <45646617+Senthilkumarmurugan@users.noreply.github.com>
2019-03-14 19:17:25 +0530

    Update bank.py

--- bank.py
+++ bank.py
@@ -11,6 +11,8 @@ class bank:
     def deposit():
 
         deposit=float(input("Enter the amount to deposit:"))
+        # self.balance=self.balance+amount
+        # print("The balance in your account is ", self.balance)
         query="update cus_accountinfo set balance=balance+{} where accountno={}"
         cursor.execute(query.format(deposit,c1.accountno))
         conn.commit()
@@ -29,7 +31,7 @@ class bank:
         cursor.execute(query.format(c1.accountno))
         row = cursor.fetchone()
         while row:
-            print(balance.append(row[0]))
+            balance.append(row[0])
             row = cursor.fetchone()
         if(withdraw<balance[0]):
             query="update cus_accountinfo set balance=balance-{} where accountno={}"
@@ -86,6 +88,31 @@ class bank:
             print("Accountno:{} \nName:{}\nPhone:{}\nAddress:{}\nPassword:{}\nBalance:{}\nPin:{}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6]))
             row = cursor.fetchone()
 
+    @staticmethod
+    def accounttransfer():
+        amount=float(input("Enter the amount to transfer:"))
+        accountno=int(input("Enter the account number:"))
+        # withdraw account
+        balance=[]
+        query="select balance from cus_accountinfo where accountno={}"
+        cursor.execute(query.format(c1.accountno))
+        row = cursor.fetchone()
+        while row:
+            balance.append(row[0])
+            row = cursor.fetchone()
+        if(amount<balance[0]):
+            query="update cus_accountinfo set balance=balance-{} where accountno={}"
+            cursor.execute(query.format(amount,c1.accountno))
+            conn.commit()
+        else:
+            print("insufficient fund")
+        # deposit account
+        query="update cus_accountinfo set balance=balance+{} where accountno={}"
+        cursor.execute(query.format(amount,accountno))
+        conn.commit()
+        print("amount transfered successfully ")
+
+
 
 
     def newregistration(self):
@@ -139,7 +166,7 @@ class bank:
                 for i in result:
                     print("Welcome ",i[1])
                     while True:
-                        print("B---->Show balance\nD---->deposit\nW---->Withdraw\nP---->Pin\nCP---->Change ATM pin\nI---->Customer info\nE---->Exit")
+                        print("B---->Show balance\nD---->deposit\nW---->Withdraw\nP---->Pin\nCP---->Change ATM pin\nI---->Info\nT---->Account transfer\nE---->Exit")
                         option=input("Enter the option:")
                         if(option.upper()=='D'):
                             c1.deposit()
@@ -156,6 +183,8 @@ class bank:
                             c1.showpin()
                         elif(option.upper()=='I'):
                             c1.cusinfo()
+                        elif(option.upper()=='T'):
+                            c1.accounttransfer()
                         else:
                             print("Invalid option")
 
@@ -163,9 +192,7 @@ class bank:
                 print("Accountno and Password not match Try again....")
         except ValueError:
             print("Accountno must be in numeric")
-    # @staticmethod
-    # def deposit():
-    #     query="select from "
+
 c1=bank()
 
 
@@ -180,22 +207,124 @@ try:
     print("Welcome to ",bank.bname)
 
     while True:
-        print("E---->Existing user\nN---->New registration\nC---->Exit")
+        print("E---->Existing user\nN---->New registration")
         option=input("Enter the option:")
         if(option.upper()=='E'):
             c1.existing_user()
         elif(option.upper()=='N'):
             c1.newregistration()
-        elif(option.upper()=='c'):
-            sys.exit()
         else:
             print("not a valid option")
 
-except pyodbc.OperationalError:
-    print("Database connection failed....")
-except pyodbc.ProgrammingError:
-    print("Database not found....")
+except pyodbc.OperationalError as e:
+    print("Database connection failed....",e)
+except pyodbc.ProgrammingError as e:
+    print("Database not found.... or error in queries....",e)
 
 
 
+
+
+
+
+
+    
+ OUTPUT:
+connected successfully......
+Welcome to  Indian Bank
+E---->Existing user
+N---->New registration
+Enter the option:e
+Enter your accountno:12345678
+Enter your password:****
+Welcome  senthil
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:b
+Your balance is:10500
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:t
+Enter the amount to transfer:500
+Enter the account number:760439002
+amount transfered successfully 
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:b
+Your balance is:10000
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:d
+Enter the amount to deposit:1000
+amount successfully deposited
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:w
+Enter the amount to withdraw:1000
+amount withdrawed successfully
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:p
+Your pin is:1122
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:i
+Accountno:12345678   
+Name:senthil
+Phone:9500482272
+Address:vettavalam
+Password:****
+Balance:10000.0
+Pin:1122
+B---->Show balance
+D---->deposit
+W---->Withdraw
+P---->Pin
+CP---->Change ATM pin
+I---->Info
+T---->Account transfer
+E---->Exit
+Enter the option:
 
